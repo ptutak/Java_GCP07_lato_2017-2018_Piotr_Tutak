313e5dd4733670941f744d96f05c69d642c07501
ptutak <p.tutak@outlook.com>
2017-03-24 08:51:32 +0100

    menu, close

--- src/gui1/CustomMenuBar.java
+++ src/gui1/CustomMenuBar.java
@@ -1,7 +1,26 @@
 package gui1;
 
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 
 public class CustomMenuBar extends MenuBar {
 
+	Menu menuProgram;
+	Menu menuAbout;
+	
+	MenuItem close;
+	
+	CustomMenuBar(){
+		menuProgram=new Menu("Program");
+		close=new MenuItem("Close Ctrl+C");
+		close.setOnAction((t) -> {
+			System.exit(0);
+		});
+		menuProgram.getItems().add(close);
+		menuAbout=new Menu("About");
+		
+		this.getMenus().addAll(menuProgram,menuAbout);
+		
+	}
 }
