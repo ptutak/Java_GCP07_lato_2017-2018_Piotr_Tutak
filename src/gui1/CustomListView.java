67b39a0180a31abcce8359c1230c85b0e7c94759
ptutak <p.tutak@outlook.com>
2017-03-28 06:58:20 +0200

    ok

---
+++ src/gui1/CustomListView.java
@@ -0,0 +1,35 @@
+package gui1;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import kolekcje_i_algorytmy.Student;
+
+public class CustomListView extends AnchorPane{
+	
+	private ObservableList<String> logList = FXCollections.observableArrayList();
+	private ListView<String> listView;
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	
+	CustomListView(){
+		 listView = new ListView<String>(logList);
+		 listView.setMinWidth(562);
+		 this.getChildren().add(listView);		 
+	}
+	public void addedStudent(Student student){
+		logList.add(dateFormat.format(new Date())+":  ADDED:  "+student);
+	}
+	public void removedStudent(Student student){
+		logList.add(dateFormat.format(new Date())+":  REMOVED:  "+student);
+	}
+	public void notModifiedStudent(Student student){
+		logList.add(dateFormat.format(new Date())+":  NOT MODIFIED:  "+student);
+	}
+	
+}
---
+++ src/gui1/CustomTabPane.java
@@ -0,0 +1,58 @@
+package gui1;
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import kolekcje_i_algorytmy.Student;
+
+public class CustomTabPane extends AnchorPane{
+	private CustomListView logList=new CustomListView();
+	private CustomTableView studentsList=new CustomTableView();
+	private CustomBarChart histList=new CustomBarChart();
+	
+	TabPane tabs;
+	Tab students;
+	Tab log;
+	Tab hist;
+	
+	CustomTabPane(){
+		 tabs = new TabPane();
+		 
+		 students=new Tab();
+		 students.setText("Students");
+		 students.setContent(studentsList);
+		 
+		 log = new Tab();
+		 log.setText("Log");
+		 log.setContent(logList);
+		 
+		 hist=new Tab();
+		 hist.setText("Histogram");
+		 hist.setContent(histList);
+		 
+		 tabs.getTabs().add(students);
+		 tabs.getTabs().add(log);
+		 tabs.getTabs().add(hist);
+		 this.getChildren().add(tabs);
+	}
+	
+	public void addStudent(Student student){
+		studentsList.addStudent(student);
+		logList.addedStudent(student);
+		histList.addMark(student.getMark());
+	}
+	
+	public void removeStudent(Student student){
+		studentsList.removeStudent(student);
+		logList.removedStudent(student);
+		histList.removeMark(student.getMark());
+	}
+	
+	public void notModifiedStudent(Student student){
+		logList.notModifiedStudent(student);
+	}
+
+
+}
