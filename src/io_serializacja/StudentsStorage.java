cdaba728ee02830a412ce174eb9baef91628dd9e
Piotr Tutak <p.tutak@outlook.com>
2017-05-08 17:07:36 +0200

    changes

--- src/io_serializacja/StudentsStorage.java
+++ src/io_serializacja/StudentsStorage.java
@@ -48,7 +48,8 @@ public class StudentsStorage {
 		dis.skip(index*12);
 		int offset=dis.readInt();
 		int size=dis.readInt();
-		offset-=12*index+8;
+		dis.readInt();
+		offset-=12*(index+1);
 		dis.skip(offset);
 		byte[] image=new byte[size];
 		int readBytes=dis.read(image);
@@ -65,15 +66,14 @@ public class StudentsStorage {
 
 		fileInputStream=new FileInputStream(file);
 		dis=new DataInputStream(fileInputStream);
-		
 		dis.skip(index*12);
 		int offset=dis.readInt();
 		int size=dis.readInt();
-		int new_offset=offset-20*4;
+		int new_offset=offset-20*12;
 		int tmp=dis.readInt();
 		tmp=0;
 		int imagesSize=0;
-		for (int i=index+1;i<20;++i){
+		for (int i=index;i<20;++i){
 			tmp=dis.readInt();
 			tmp=dis.readInt();
 			imagesSize+=tmp;
@@ -83,15 +83,31 @@ public class StudentsStorage {
 		dis.skip(new_offset+size);
 		
 		byte[] images=new byte[imagesSize];
+		dis.read(images);
+		
 		if (images.length!=imagesSize)
 			System.out.println("wrong size put");
 
 		dis.close();
 		fileInputStream.close();
-		fileOutputStream=new FileOutputStream(file);
-		dos=new DataOutputStream(fileOutputStream);
-		
-		
+
+		raf=new RandomAccessFile(file, "rw");
+		raf.seek(12*index);
+		raf.writeInt(offset);
+		raf.writeInt(image.length);
+		raf.writeInt(0);
+		int off=offset+image.length;
+		for (int i=index+1;i<20;++i){
+			raf.writeInt(off);
+			off+=raf.readInt();
+			raf.readInt();
+		}
+
+		raf.seek(offset);
+		raf.write(image);
+		raf.write(images);
+		raf.close();
+				
 	}
 	
 	public void close() throws IOException{
